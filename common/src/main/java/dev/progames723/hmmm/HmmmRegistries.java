1da2b8e4cdf6fcdbf8ba29dd9bf82252b8f4d865
Progames723 <93779034+Progames723@users.noreply.github.com>
2024-03-17 16:05:14 +0700

    changed HmmmRegistries a bit

--- common/src/main/java/dev/progames723/hmmm/HmmmRegistries.java
+++ common/src/main/java/dev/progames723/hmmm/HmmmRegistries.java
@@ -1,5 +1,6 @@
 package dev.progames723.hmmm;
 
+import dev.architectury.platform.Platform;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -20,37 +21,52 @@ import org.slf4j.Logger;
  */
 public class HmmmRegistries {
 	public static MobEffect registerEffect(String modid, String name, MobEffect obj, @Nullable Logger logger) {
-		if (logger != null) {
-			logger.info("Registered effect: " + name);
+		if (!Platform.isForgeLike()){
+			if (logger != null) {
+				logger.info("Registered effect: " + name);
+			}
+			return Registry.register(BuiltInRegistries.MOB_EFFECT, new ResourceLocation(modid, name), obj);
 		}
-		return Registry.register(BuiltInRegistries.MOB_EFFECT, new ResourceLocation(modid, name), obj);
+		throw new RuntimeException("Fabric and Quilt only!");
 	}
 
 	public static Item registerItem(String modid, String name, Item obj, @Nullable Logger logger) {
-		if (logger != null) {
-			logger.info("Registered item: " + name);
+		if (!Platform.isForgeLike()){
+			if (logger != null) {
+				logger.info("Registered item: " + name);
+			}
+			return Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(modid, name), obj);
 		}
-		return Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(modid, name), obj);
+		throw new RuntimeException("Fabric and Quilt only!");
 	}
 
 	public static Enchantment registerEnchantment(String modid, String name, Enchantment obj, @Nullable Logger logger) {
-		if (logger != null) {
-			logger.info("Registered enchantment: " + name);
+		if (!Platform.isForgeLike()){
+			if (logger != null) {
+				logger.info("Registered enchantment: " + name);
+			}
+			return Registry.register(BuiltInRegistries.ENCHANTMENT, new ResourceLocation(modid, name), obj);
 		}
-		return Registry.register(BuiltInRegistries.ENCHANTMENT, new ResourceLocation(modid, name), obj);
+		throw new RuntimeException("Fabric and Quilt only!");
 	}
 
 	public static Attribute registerAttribute(String modid, String name, Attribute obj, @Nullable Logger logger) {
-		if (logger != null) {
-			logger.info("Registered attribute: " + name);
+		if (!Platform.isForgeLike()){
+			if (logger != null) {
+				logger.info("Registered attribute: " + name);
+			}
+			return Registry.register(BuiltInRegistries.ATTRIBUTE, new ResourceLocation(modid, name), obj);
 		}
-		return Registry.register(BuiltInRegistries.ATTRIBUTE, new ResourceLocation(modid, name), obj);
+		throw new RuntimeException("Fabric and Quilt only!");
 	}
 	public static Block registerBlock(String modid, String name, Block obj, @Nullable Logger logger) {
-		if (logger != null) {
-			logger.info("Registered attribute: " + name);
+		if (!Platform.isForgeLike()){
+			if (logger != null) {
+				logger.info("Registered block: " + name);
+			}
+			return Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(modid, name), obj);
 		}
-		return Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(modid, name), obj);
+		throw new RuntimeException("Fabric and Quilt only!");
 	}
 
 	public static ResourceKey<DamageType> registerDamageType(String modid, String name, @Nullable Logger logger) {
