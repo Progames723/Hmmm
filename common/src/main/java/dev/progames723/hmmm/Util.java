584a8d12a48cfe65d023e28199c8e724f1ae5d4b
Progames723 <93779034+Progames723@users.noreply.github.com>
2024-04-21 12:42:43 +0700

    made Util.java a lot better and added extra methods

--- common/src/main/java/dev/progames723/hmmm/Util.java
+++ common/src/main/java/dev/progames723/hmmm/Util.java
@@ -1,19 +1,21 @@
 package dev.progames723.hmmm;
 
+import org.jetbrains.annotations.Range;
+
 import java.util.Random;
 
 public class Util {
 	private Util() {}//no instances allowed
-	
-	public static int iPercent(int number, int max) {return (int)lPercent(number, max);}
 
-	public static long lPercent(long number, long max) {return (number / max) * 100;}
+	public static long percent(long number, long max) {return (number / max) * 100;}
 	
-	public static double dPercent(double number, double max) {return (number / max) * 100;}
+	public static int percent(int number, int max) {return (int) percent((long) number, max);}
 	
-	public static float fPercent(float number, float max) {return (float)dPercent(number, max);}
+	public static double percent(double number, double max) {return (number / max) * 100;}
 	
-	public static boolean dChance(double percent) {
+	public static float percent(float number, float max) {return (float)percent((double) number, max);}
+	
+	public static boolean chance(double percent) {
 		Random rand = new Random();
 		double i = rand.nextDouble();
 		if (percent > 100) percent = 100;
@@ -22,27 +24,47 @@ public class Util {
 		return i <= percent;
 	}
 
-	public static boolean fChance(float percent) {return dChance(percent);}
+	public static boolean chance(float percent) {return chance((double) percent);}
 	
-	public static boolean lChance(long percent) {
+	public static boolean chance(long percent) {
 		Random rand = new Random();
 		int i = rand.nextInt(101);
 		return i <= percent;
 	}
 	
-	public static boolean iChance(int percent) {return lChance(percent);}
+	public static boolean chance(int percent) {return chance((long) percent);}
 	
-	public static long lRandomRange(long min, long max) {
+	public static long randomRange(long min, long max) {
 		Random rand = new Random();
 		return rand.nextLong((max - min) + 1) + min;
 	}
 	
-	public static int iRandomRange(int min, int max) {return (int)lRandomRange(min, max);}
+	public static int randomRange(int min, int max) {return (int) randomRange((long) min, max);}
 	
-	public static double dRandomRange(double min, double max) {
+	public static double randomRange(double min, double max) {
 		Random rand = new Random();
 		return rand.nextDouble((max - min) + 1) + min;
 	}
 	
-	public static float fRandomRange(float min, float max) {return (float)dRandomRange(min, max);}
+	public static float randomRange(float min, float max) {return (float) randomRange((double) min, max);}
+	
+	public static double unExponentialFormula(double value, double divisor, double maxValue) {//great name
+		if (value < 0) throw new IllegalArgumentException("value must be higher than 0");
+		else if (value == 0) return 0.0;
+		double output = (1-(1/(1+value/divisor))) * maxValue;
+		output = roundTo(output, 3);//rounding to the 0.001!
+		return output;
+	}
+	
+	public static double roundTo(double value, @Range(from = 0, to = Long.MAX_VALUE) long powerOf10) {//no way this actually works
+		return Math.round(value * Math.pow(10.0, powerOf10)) / Math.pow(10.0, powerOf10);
+	}
+	
+	public static float unExponentialFormula(float value, float divisor, float maxValue) {
+		return (float) unExponentialFormula((double) value, divisor, maxValue);
+	}
+	
+	public static float roundTo(float value, @Range(from = 0, to = Long.MAX_VALUE) long powerOf10) {
+		return (float) roundTo((double) value, powerOf10);
+	}
 }
